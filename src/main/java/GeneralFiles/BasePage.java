4b7b70aaf430de7c67938d982dd67e11aa94ed09
iremucr <iremremucr@gmail.com>
2025-07-30 14:00:19 +0300

    BasePage Güncellemesi

--- src/main/java/GeneralFiles/BasePage.java
+++ src/main/java/GeneralFiles/BasePage.java
@@ -2,7 +2,14 @@ package GeneralFiles;
 
 import BaseFiles.DriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.time.Duration;
 
 public class BasePage {
     WebDriver driver=null;
@@ -11,10 +18,28 @@ public class BasePage {
         driver= DriverManager.getDriver();
     }
 
+    public WebElement waitUntillClickable(By locator){
+        try {
+            Wait<WebDriver> wait = new FluentWait<>(driver)
+
+                    .withTimeout(Duration.ofSeconds(30)).withTimeout(Duration.ofSeconds(30))
+
+                    .pollingEvery(Duration.ofSeconds(5)).pollingEvery(Duration.ofSeconds(5))
+
+                    .ignoring(NoSuchElementException.class).ignoring(NoSuchElementException.class);
+
+            return  wait.until(ExpectedConditions.elementToBeClickable(locator));
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
 
     public void  clickElement(By locator){
         try {
-            driver.findElement(locator).click();
+            waitUntillClickable(locator).click();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -22,3 +47,4 @@ public class BasePage {
 
 
 }
+ 
